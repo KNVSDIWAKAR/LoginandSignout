d47a3c61cf091f8b18011015f5fca1c30e547049
KNVSDIWAKAR <divvukancherla630@gmail.com>
2024-09-02 12:53:30 +0530

    first commit

---
+++ src/components/authentication/Signup.js
@@ -0,0 +1,140 @@
+import React from "react";
+import signupImage from "../asserts/LoginBackground.jpg";
+import { Link } from "react-router-dom";
+
+const Signup = () => {
+  return (
+    <div
+      className="flex min-h-full w-full flex-1 flex-col justify-center px-6 py-12 lg:px-8"
+      //   style={{
+      //     backgroundImage: `url(${signupImage})`,
+      //     backgroundSize: "cover",
+      //     backgroundPosition: "center",
+      //     backgroundRepeat: "no-repeat",
+      //     height: "100vh",
+      //   }}
+    >
+      <div className="mt-20 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form action="#" method="POST" className="space-y-6">
+          <div>
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Name
+            </label>
+            <div className="mt-2">
+              <input
+                id="name"
+                name="name"
+                type="text"
+                required
+                autoComplete="name"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <label
+              htmlFor="mobile"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Mobile Number
+            </label>
+            <div className="mt-2">
+              <input
+                id="mobile"
+                name="mobile"
+                type="tel"
+                pattern="[0-9]{10}" // Ensures 10-digit numbers only
+                required
+                autoComplete="tel"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <label
+              htmlFor="age"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Age
+            </label>
+            <div className="mt-2">
+              <input
+                id="age"
+                name="age"
+                type="number"
+                min="1"
+                required
+                autoComplete="age"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                required
+                autoComplete="email"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Password
+            </label>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                required
+                autoComplete="new-password"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
+
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Already have an account?{" "}
+          <Link
+            to="/login"
+            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+          >
+            Login
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
